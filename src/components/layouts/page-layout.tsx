2149b1d467db0ee73be41611e78a939a208997ca
DrShpongle <evgeniy.nagalskiy@gmail.com>
2022-01-11 14:59:38 +0200

    Proud to be partnered with

--- src/components/layouts/page-layout.tsx
+++ src/components/layouts/page-layout.tsx
@@ -2,12 +2,18 @@ import * as React from 'react'
 
 import Header from 'components/app/header'
 import Footer from 'components/app/footer'
+import Partnered from 'components/partnered'
 
 const PageLayout: React.FC<{children: React.ReactNode}> = ({children}) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="grow">{children}</main>
+      <div className="mb-16">
+        <div className="container">
+          <Partnered />
+        </div>
+      </div>
       <Footer />
     </div>
   )
---
+++ src/components/partnered.tsx
@@ -0,0 +1,88 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+const Partnered = () => {
+  return (
+    <div>
+      <style jsx>
+        {`
+          .cell-with-border {
+            box-shadow: 0 0 0 1px black;
+          }
+        `}
+      </style>
+      <h3 className="text-[2.5rem]">Proud to be partnered with&hellip;</h3>
+      <div className="grid grid-cols-5 mt-8 gap-[1px] p-[1px]">
+        {partneredLogos.map((logo, index) => {
+          return (
+            <a
+              key={index}
+              className="block p-8 cell-with-border"
+              href={logo.url}
+              target="_blank"
+            >
+              <div className="relative aspect-square">
+                <Image src={logo.path} layout="fill" objectFit="contain" />
+              </div>
+            </a>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+const partneredLogos = [
+  {
+    title: 'Red Bull',
+    path: '/images/partnered/red-bull.svg',
+    url: 'https://www.redbull.com/',
+  },
+  {
+    title: 'Blenders Eyewear',
+    path: '/images/partnered/blenders-eyewear.svg',
+    url: 'https://www.blenderseyewear.com/',
+  },
+  {
+    title: 'GoPro',
+    path: '/images/partnered/gopro-hero.svg',
+    url: 'https://gopro.com/',
+  },
+  {
+    title: 'Cariuma',
+    path: '/images/partnered/cariuma.svg',
+    url: 'https://cariuma.com/',
+  },
+  {
+    title: 'Catch Surf',
+    path: '/images/partnered/catch-surf.png',
+    url: 'https://catchsurf.com/',
+  },
+  {
+    title: 'Murf Electric Bikes',
+    path: '/images/partnered/murf-electric-bikes.png',
+    url: 'https://murfelectricbikes.com/',
+  },
+  {
+    title: 'Buell Surf',
+    path: '/images/partnered/buell-surf.png',
+    url: 'https://buellsurf.com/',
+  },
+  {
+    title: 'Tokoro',
+    path: '/images/partnered/kerry-tokoro.png',
+    url: 'http://www.tokorosurfboards.com/',
+  },
+  {
+    title: 'Vestal Watches',
+    path: '/images/partnered/vestal.png',
+    url: 'https://vestalwatch.com.au/',
+  },
+  {
+    title: 'Tools Surf Products',
+    path: '/images/partnered/tools.png',
+    url: 'http://www.tools-international.com/',
+  },
+]
+
+export default Partnered
