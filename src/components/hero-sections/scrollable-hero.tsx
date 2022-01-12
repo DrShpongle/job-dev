02b555600742b4a2c92144a8e7a9f2d395c4dcb4
DrShpongle <evgeniy.nagalskiy@gmail.com>
2022-01-12 10:29:54 +0200

    clean up

--- src/components/hero-sections/scrollable-hero.tsx
+++ src/components/hero-sections/scrollable-hero.tsx
@@ -31,44 +31,53 @@ const ScrollableHero: React.FC = () => {
   })
 
   return (
-    <div
-      className="relative flex justify-center w-full overflow-hidden flex-nowrap touch-none full-screen-height"
-      onWheel={changeWidth}
-      onTouchMove={(e) => console.log('e:', e)}
-    >
-      <div className="relative w-1/2 overflow-hidden shrink-0">
-        <VideoEmbed
-          src="https://cdn.videvo.net/videvo_files/video/free/2021-04/large_watermarked/210329_06B_Bali_1080p_013_preview.mp4"
-          className="absolute left-0"
-        />
-      </div>
+    <>
+      <style jsx>
+        {`
+          .full-screen-height {
+            height: calc(100vh - 80px);
+          }
+        `}
+      </style>
       <div
-        className="absolute top-0 z-10 flex justify-center w-1/3 h-full overflow-hidden"
-        style={{width: `${currentWidth}%`}}
+        className="relative flex justify-center w-full overflow-hidden flex-nowrap touch-none full-screen-height"
+        onWheel={changeWidth}
+        onTouchMove={(e) => console.log('e:', e)}
       >
-        <VideoEmbed
-          src="https://cdn.videvo.net/videvo_files/video/free/2021-04/large_watermarked/210329_01B_Bali_1080p_014_preview.mp4"
-          className="absolute"
-        />
-        <div className="absolute bottom-0 flex flex-col items-center space-y-4 text-xl text-white">
-          <span>scroll down</span>
-          <div className="w-px h-10 bg-white" />
+        <div className="relative w-1/2 overflow-hidden shrink-0">
+          <VideoEmbed
+            src="https://cdn.videvo.net/videvo_files/video/free/2021-04/large_watermarked/210329_06B_Bali_1080p_013_preview.mp4"
+            className="absolute left-0"
+          />
+        </div>
+        <div
+          className="absolute top-0 z-10 flex justify-center w-1/3 h-full overflow-hidden"
+          style={{width: `${currentWidth}%`}}
+        >
+          <VideoEmbed
+            src="https://cdn.videvo.net/videvo_files/video/free/2021-04/large_watermarked/210329_01B_Bali_1080p_014_preview.mp4"
+            className="absolute"
+          />
+          <div className="absolute bottom-0 flex flex-col items-center space-y-4 text-xl text-white">
+            <span>scroll down</span>
+            <div className="w-px h-10 bg-white" />
+          </div>
+        </div>
+        <div className="relative w-1/2 overflow-hidden shrink-0">
+          <VideoEmbed
+            src="https://cdn.videvo.net/videvo_files/video/free/2021-04/large_watermarked/210329_13B_Bali_1080p_005_preview.mp4"
+            className="absolute right-0"
+          />
+        </div>
+        <div className="absolute right-0 z-20 flex flex-col items-center justify-center h-32 p-3 text-xl text-center text-white uppercase rounded-l-full bottom-20 w-80 bg-blue font-headings shadow-[5px_10px_6px_0px_rgba(0,0,0,0.5)]">
+          <div>win a trip to the</div>
+          <div className="absolute text-6xl text-pink font-accented">
+            Maldives
+          </div>
+          <div className="mt-10">with Jamie</div>
         </div>
       </div>
-      <div className="relative w-1/2 overflow-hidden shrink-0">
-        <VideoEmbed
-          src="https://cdn.videvo.net/videvo_files/video/free/2021-04/large_watermarked/210329_13B_Bali_1080p_005_preview.mp4"
-          className="absolute right-0"
-        />
-      </div>
-      <div className="absolute right-0 z-20 flex flex-col items-center justify-center h-32 p-3 text-xl text-center text-white uppercase rounded-l-full bottom-20 w-80 bg-blue font-headings shadow-[5px_10px_6px_0px_rgba(0,0,0,0.5)]">
-        <div>win a trip to the</div>
-        <div className="absolute text-6xl text-pink font-accented">
-          Maldives
-        </div>
-        <div className="mt-10">with Jamie</div>
-      </div>
-    </div>
+    </>
   )
 }
 
--- src/components/video-embed.tsx
+++ src/components/video-embed.tsx
@@ -6,23 +6,6 @@ const VideoEmbed: React.FC<{src: string; className?: string}> = ({
   className,
 }) => (
   <>
-    {/* <style jsx>
-      {`
-        .video-responsive {
-          overflow: hidden;
-          padding-bottom: 56.25%;
-          position: relative;
-          height: 0;
-        }
-        .video-responsive video {
-          left: 0;
-          top: 0;
-          height: 100%;
-          width: 100%;
-          position: absolute;
-        }
-      `}
-    </style> */}
     <video
       src={src}
       controls={false}
