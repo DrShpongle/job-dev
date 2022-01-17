a68c2e41485766eb90b147b6375f294861110b93
DrShpongle <evgeniy.nagalskiy@gmail.com>
2022-01-17 17:27:22 +0200

    play with animation

--- src/components/sections/be-psyched.tsx
+++ src/components/sections/be-psyched.tsx
@@ -4,17 +4,38 @@ import {motion, useViewportScroll, useTransform} from 'framer-motion'
 import {useRefScrollProgress} from 'hooks/useRefScrollProgress'
 
 const BePsyched = () => {
-  const ref = React.useRef<HTMLDivElement>(null)
-  const {start, end} = useRefScrollProgress(ref)
+  const refBoards = React.useRef<HTMLDivElement>(null)
+  const {start, end} = useRefScrollProgress(refBoards)
+  console.log('start:', start)
+  console.log('end:', end)
   const {scrollYProgress} = useViewportScroll()
   const x = useTransform(scrollYProgress, [start, end], ['-90%', '0%'])
 
+  const opacity = useTransform(scrollYProgress, [start, end], [0, 1])
+  const rotate = useTransform(scrollYProgress, [start, end], [0, 720])
+
+  const handlerScroll = () => {
+    const rect = refBoards.current.getBoundingClientRect()
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop
+    console.log('scrollTop:', scrollTop)
+  }
+
+  React.useEffect(() => {
+    window.addEventListener('scroll', handlerScroll)
+  }, [])
+
   return (
-    <section ref={ref} className="relative overflow-hidden bg-blue py-36">
+    <section ref={refBoards} className="relative overflow-hidden bg-blue py-36">
       <motion.div
-        className="absolute top-0 -translate-y-6"
-        initial={{x: '-90%'}}
-        style={{x}}
+        className="absolute top-0"
+        initial={{x: '-100%'}}
+        style={{x, y: '-2rem'}}
+        transition={{
+          type: 'spring',
+          damping: 10,
+          mass: 0.75,
+          stiffness: 100,
+        }}
       >
         <Image
           src="/images/windsurfing-boards.png"
@@ -24,7 +45,17 @@ const BePsyched = () => {
         />
       </motion.div>
       <div className="container">
-        <div className="flex justify-end">
+        <div className="flex flex-col lg:flex-row">
+          <div className="relative flex justify-end pt-48 pr-24 grow">
+            <motion.div className="shrink-0" style={{opacity, rotate}}>
+              <Image
+                src="/images/job-surf-experience-logo.png"
+                width={487}
+                height={492}
+                priority
+              />
+            </motion.div>
+          </div>
           <div className="w-2/5 2xl:w-[45%]">
             <h3 className="font-accented text-pink text-[70px]">Be Psyched</h3>
             <h2 className="text-white text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-[111px] leading-none">
