364581bd3f25a2c7255a990fb605e1ccda74cadc
DrShpongle <evgeniy.nagalskiy@gmail.com>
2022-01-18 12:41:48 +0200

    add ofset prop

--- src/hooks/useRefScrollProgress.ts
+++ src/hooks/useRefScrollProgress.ts
@@ -1,6 +1,6 @@
 import * as React from 'react'
 
-export const useRefScrollProgress = (inputRef: any) => {
+export const useRefScrollProgress = (inputRef: any, offset: number = 0) => {
   const ref = inputRef || React.useRef()
   const [start, setStart] = React.useState(0)
   const [end, setEnd] = React.useState(0)
@@ -11,10 +11,10 @@ export const useRefScrollProgress = (inputRef: any) => {
     const rect = ref.current.getBoundingClientRect()
     const scrollTop = window.pageYOffset || document.documentElement.scrollTop
     const computedStart =
-      (rect.top + scrollTop - window.innerHeight + 300) /
+      (rect.top + scrollTop - window.innerHeight + offset) /
       document.body.clientHeight
     const computedEnd =
-      (rect.bottom + scrollTop - window.innerHeight + 300) /
+      (rect.bottom + scrollTop - window.innerHeight + offset) /
       document.body.clientHeight
     setStart(computedStart)
     setEnd(computedEnd)
