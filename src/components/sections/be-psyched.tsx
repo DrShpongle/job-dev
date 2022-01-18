710023eb7e5989437fa01fe5663e0ebe5f7779cf
DrShpongle <evgeniy.nagalskiy@gmail.com>
2022-01-18 10:32:24 +0200

    BePsyched: mobile styles

--- src/components/sections/be-psyched.tsx
+++ src/components/sections/be-psyched.tsx
@@ -1,46 +1,65 @@
 import * as React from 'react'
 import Image from 'next/image'
 import {motion, useViewportScroll, useTransform} from 'framer-motion'
+import {useWindowSize} from 'react-use'
+
 import {useRefScrollProgress} from 'hooks/useRefScrollProgress'
 
 const BePsyched = () => {
+  const [isMounted, setIsMounted] = React.useState<boolean>(false)
+  const {width, height} = useWindowSize()
   const refBoards = React.useRef<HTMLDivElement>(null)
   const {start, end} = useRefScrollProgress(refBoards)
   const {scrollYProgress} = useViewportScroll()
-  const x = useTransform(scrollYProgress, [start, end], ['-90%', '0%'])
+  const rangeX = useTransform(scrollYProgress, [start, end], ['-90%', '0%'])
 
   const opacity = useTransform(scrollYProgress, [start, end], [0, 1])
   const rotate = useTransform(scrollYProgress, [start, end], [0, 720])
 
+  React.useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
   return (
     <section
       ref={refBoards}
-      className="relative overflow-hidden bg-blue lg:py-20 xl:py-28 2xl:py-36"
+      className="relative pt-8 overflow-hidden md:pt-10 bg-blue lg:py-20 xl:py-28 2xl:py-36"
     >
-      <motion.div
-        className="absolute top-0 lg:w-[450px] xl:w-[500px] 2xl:w-[630px]"
-        initial={{x: '-90%'}}
-        style={{x, y: '-2rem'}}
-        transition={{
-          type: 'spring',
-          damping: 10,
-          mass: 0.75,
-          stiffness: 100,
-        }}
-      >
-        <Image
-          src="/images/windsurfing-boards.png"
-          width={630}
-          height={1190}
-          priority
-        />
-      </motion.div>
+      {isMounted && (
+        <motion.div
+          className="absolute bottom-0 w-full lg:top-0 lg:w-[450px] xl:w-[500px] 2xl:w-[630px] flex"
+          initial={width >= 1024 ? {x: '-90%'} : {}}
+          style={width >= 1024 ? {x: rangeX} : {}}
+          transition={{
+            type: 'spring',
+            damping: 10,
+            mass: 0.75,
+            stiffness: 100,
+          }}
+        >
+          {width >= 1024 ? (
+            <Image
+              src="/images/windsurfing-boards-vertical.png"
+              width={630}
+              height={1190}
+              priority
+            />
+          ) : (
+            <Image
+              src="/images/windsurfing-boards-horizontal.png"
+              width={1536}
+              height={813}
+              priority
+            />
+          )}
+        </motion.div>
+      )}
       <div className="container">
-        <div className="flex flex-col lg:flex-row">
-          <div className="relative flex items-start justify-end pt-48 lg:pr-8 xl:pr-16 2xl:pr-24 grow">
+        <div className="flex flex-col lg:flex-row pb-[250px] md:pb-[500px] lg:pb-0">
+          <div className="relative flex items-start justify-end lg:pt-48 lg:pr-8 xl:pr-16 2xl:pr-24 grow">
             <motion.div
-              className="origin-center shrink-0 lg:w-[250px] xl:w-[350px] 2xl:w-[487px]"
-              style={{opacity, rotate}}
+              className="absolute origin-center shrink-0 w-32 md:w-56 lg:w-[250px] xl:w-[350px] 2xl:w-[487px]"
+              style={width >= 1024 ? {opacity, rotate} : {}}
             >
               <Image
                 src="/images/job-surf-experience-logo.png"
@@ -50,25 +69,29 @@ const BePsyched = () => {
               />
             </motion.div>
           </div>
-          <div className="lg:w-2/5 xl:w-[45%]">
-            <h3 className="font-accented text-pink text-[70px]">Be Psyched</h3>
-            <h2 className="text-white text-5xl md:text-6xl lg:text-6xl xl:text-7xl 2xl:text-[111px] leading-none">
+          <div className="lg:w-2/5 xl:w-[45%] pt-12 md:pt-16 lg:pt-0">
+            <h3 className="font-accented text-pink text-3xl md:text-6xl lg:text-[70px]">
+              Be Psyched
+            </h3>
+            <h2 className="text-white text-5xl md:text-7xl lg:text-6xl xl:text-7xl 2xl:text-[111px] leading-none">
               with The
               <br />
-              Jamie O’Brien Experience
+              Jamie O’Brien
+              <br />
+              Experience
             </h2>
-            <p className="mt-12 lg:text-2xl lg:leading-normal xl:text-3xl xl:leading-normal 2xl:text-[34px] 2xl:leading-normal text-white">
+            <p className="mt-8 md:mt-12 md:text-2xl md:leading-normal xl:text-3xl xl:leading-normal 2xl:text-[34px] 2xl:leading-normal text-white">
               Expliqua sitibusa pe nullest, velitiust porerum vel escipsamusae
               nem nonsedit, utestiam, sus quia quis doluptio illatem et aut
               optat quam nam nimagnis doloreh enistorro.
             </p>
-            <div className="flex items-center mt-8 lg:space-x-6 xl:space-x-10">
-              <button className="lg:px-6 xl:px-10 py-4 text-white uppercase rounded-full bg-pink font-headings text-lg md:text-xl lg:text-lg xl:text-2xl 2xl:text-[29px]">
+            <div className="relative flex items-center mt-8 space-x-6 xl:space-x-10">
+              <button className="px-6 xl:px-10 py-2 md:py-4 text-white uppercase rounded-full bg-pink font-headings md:text-xl lg:text-lg xl:text-2xl 2xl:text-[29px]">
                 Book your experience
               </button>
               <a
                 href="#"
-                className="flex items-center space-x-1 lg:text-xl xl:text-[29px] text-pink font-headings whitespace-nowrap"
+                className="flex items-center space-x-1 md:text-2xl lg:text-xl xl:text-[29px] text-pink font-headings whitespace-nowrap"
               >
                 <span>Learn more</span>
                 <span className="translate-y-0.5">&#62;</span>
