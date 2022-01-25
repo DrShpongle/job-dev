fdd4a7efcbe93f1c68fc74fff09997c2cee580dc
DrShpongle <evgeniy.nagalskiy@gmail.com>
2022-01-25 18:12:07 +0200

    spme progress

--- src/hooks/useRefScrollProgress.ts
+++ src/hooks/useRefScrollProgress.ts
@@ -2,22 +2,17 @@ import * as React from 'react'
 
 export const useRefScrollProgress = (inputRef: any, offset: number = 0) => {
   const ref = inputRef || React.useRef()
-  const [start, setStart] = React.useState(0)
-  const [end, setEnd] = React.useState(0)
-  React.useEffect(() => {
+  const [start, setStart] = React.useState(null)
+  const [end, setEnd] = React.useState(null)
+  React.useLayoutEffect(() => {
     if (!ref.current) {
       return
     }
     const rect = ref.current.getBoundingClientRect()
     const scrollTop = window.pageYOffset || document.documentElement.scrollTop
-    const computedStart =
-      (rect.top + scrollTop - window.innerHeight + offset) /
-      document.body.clientHeight
-    const computedEnd =
-      (rect.bottom + scrollTop - window.innerHeight + offset) /
-      document.body.clientHeight
-    setStart(computedStart)
-    setEnd(computedEnd)
+    const offsetTop = rect.top + scrollTop
+    setStart(offsetTop / document.body.clientHeight)
+    setEnd((offsetTop + rect.height) / document.body.clientHeight)
   })
   return {ref, start, end}
 }
