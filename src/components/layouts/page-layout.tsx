ab094798cc2c55eeab483a3b3eac5d5da5c0e215
DrShpongle <evgeniy.nagalskiy@gmail.com>
2022-01-29 17:50:47 +0200

    fix ad label

--- src/components/layouts/page-layout.tsx
+++ src/components/layouts/page-layout.tsx
@@ -3,6 +3,7 @@ import * as React from 'react'
 import Header from 'components/app/header'
 import Footer from 'components/app/footer'
 import Partnered from 'components/sections/partnered'
+import FixedLabel from 'components/fixed-label'
 
 const PageLayout: React.FC<{children: React.ReactNode}> = ({children}) => {
   return (
@@ -12,6 +13,12 @@ const PageLayout: React.FC<{children: React.ReactNode}> = ({children}) => {
       <div className="relative z-10">
         <Partnered />
         <Footer />
+        <FixedLabel
+          title="Maldives"
+          subtitleTop="win a trip to the"
+          subtitleBottom="with Jamie"
+          className="fixed right-0 bottom-6 md:bottom-20"
+        />
       </div>
     </div>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,6 @@ import type {NextPage} from 'next'
 
 import PageLayout from 'components/layouts/page-layout'
 
-import Header from 'components/app/header'
-import Footer from 'components/app/footer'
-import Partnered from 'components/sections/partnered'
-
 import ScrollableHero from 'components/sections/home/scrollable-hero'
 import GetPsyched from 'components/sections/home/get-psyched'
 
@@ -15,7 +11,6 @@ import StayPsyched from 'components/sections/home/stay-psyched'
 import Vlog from 'components/sections/home/vlog'
 import PsychMag from 'components/sections/home/psych-mag'
 import SeaTrees from 'components/sections/home/sea-trees'
-import FixedLabel from 'components/fixed-label'
 
 const Landing: NextPage = () => {
   const [isMounted, setIsMounted] = React.useState<boolean>(false)
@@ -35,12 +30,6 @@ const Landing: NextPage = () => {
           <Vlog />
           <PsychMag />
           <SeaTrees />
-          <FixedLabel
-            title="Maldives"
-            subtitleTop="win a trip to the"
-            subtitleBottom="with Jamie"
-            className="fixed right-0 bottom-6 md:bottom-20"
-          />
         </section>
       </PageLayout>
     </>
