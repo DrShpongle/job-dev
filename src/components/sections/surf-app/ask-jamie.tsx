bd2ac02b6873c29418730a38b26a5f85dfb5bcaa
DrShpongle <evgeniy.nagalskiy@gmail.com>
2022-01-31 20:07:23 +0200

    SurfApp page: wip

--- src/components/sections/surf-app/ask-jamie.tsx
+++ src/components/sections/surf-app/ask-jamie.tsx
@@ -11,14 +11,14 @@ const AskJamie = () => {
         <h2 className="text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-[111px] leading-none">
           Ask Jamie
         </h2>
-        <div className="relative pb-16 mt-6 xl:pl-20 xl:pb-12 2xl:pb-16 xl:mt-20 2xl:mt-28">
-          <p className="left-0 md:text-xl lg:text-2xl xl:text-2xl lg:leading-normal xl:leading-normal 2xl:text-3xl 2xl:leading-normal max-w-[28rem] 2xl:max-w-[36rem] xl:absolute xl:-top-16 2xl:-top-24 z-10">
-            If you’ve always wanted to ask JOB a question, this is for you.
-            Whether it’s how to stay so focused, overcome bad habits or what do
-            I need to do to create loads of spray with my top turn? ASK AWAY.
-            You literally have Jamie in your pocket!
-          </p>
-          <div className="w-[80%] xl:w-[996px] 2xl:w-[1220px] mt-6 xl:mt-0">
+        <p className="mt-4 md:mt-8 md:text-xl lg:text-2xl xl:text-3xl lg:leading-normal xl:leading-normal 2xl:text-[34px] 2xl:leading-normal xl:max-w-4xl 2xl:max-w-5xl">
+          If you’ve always wanted to ask JOB a question, this is for you.
+          Whether it’s how to stay so focused, overcome bad habits or what do I
+          need to do to create loads of spray with my top turn? ASK AWAY. You
+          literally have Jamie in your pocket!
+        </p>
+        <div className="relative flex flex-col items-center pb-16 mt-8 md:block md:mt-10 xl:pl-20 md:pb-16 lg:pb-14 xl:pb-16 2xl:pb-20 lg:mt-12 xl:mt-16">
+          <div className="hidden md:block w-[80%] xl:w-[996px] 2xl:w-[1220px] mt-6 xl:mt-0">
             <Image
               src="/images/ask-jamie.jpg"
               width={1220}
@@ -27,7 +27,7 @@ const AskJamie = () => {
               layout="responsive"
             />
           </div>
-          <div className="w-[120px] xl:w-[280px] 2xl:w-[340px] absolute right-0 -bottom-12 xl:bottom-0 flex">
+          <div className="w-[220px] md:w-[200px] lg:w-[250px] xl:w-[280px] 2xl:w-[340px] relative md:absolute md:right-0 md:bottom-0 flex">
             <Image
               src="/images/iphone-frame-portrait.png"
               width={580}
@@ -35,7 +35,10 @@ const AskJamie = () => {
               alt="Ask Jamie"
               className="relative z-10"
             />
-            <div className="absolute overflow-hidden inset-1 xl:inset-4 rounded-[30%] xl:rounded-[10%]">
+            <div
+              className="absolute overflow-hidden inset-1 md:inset-2 rounded-[30px] md:rounded-[10%] xl:inset-3 2xl:inset-4 lg:rounded-[15%] 2xl:rounded-[40px]"
+              style={{transform: 'translateZ(0)'}}
+            >
               <VideoPlayer
                 url="/videos/ask-jamie-test-video.mp4"
                 // controls={true}
--- src/components/sections/surf-app/hero-with-scrolling-text.tsx
+++ src/components/sections/surf-app/hero-with-scrolling-text.tsx
@@ -32,7 +32,7 @@ const TextItem: React.FC<{text: string}> = ({text}) => {
     <div
       ref={refText}
       className={classNames(
-        'text-3xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-[111px] leading-none text-white font-headings duration-300 py-6 md:py-8 first:pt-0 last:pb-0 will-change-[opacity]',
+        'text-3xl md:text-7xl lg:text-7xl xl:text-8xl 2xl:text-[111px] leading-none text-white font-headings duration-300 py-6 md:py-8 first:pt-0 last:pb-0 will-change-[opacity]',
         active ? 'opacity-100' : 'opacity-[0.15]',
       )}
     >
@@ -76,53 +76,55 @@ const HeroWithScrollableText = () => {
 
   return (
     <section ref={refSection} className="sticky top-0 h-screen overflow-hidden">
-      <div className="absolute inset-0 before:absolute before:bg-black/40 before:inset-0">
-        <VideoEmbed url="https://mytwynmediaservices-euno.akamaized.net/45f6339a-4429-44d6-a297-ef025d31558b/45f6339a-4429-44d6-a297-ef025d31.ism/manifest(format=m3u8-aapl).m3u8" />
-      </div>
-      <motion.div
-        ref={refTextHolder}
-        className="absolute bottom-0 z-10 w-full"
-        initial={{y: textBlockHeight}}
-        transition={{
-          duration: 0.2,
-        }}
-        style={{y: scrollText}}
-      >
-        <div ref={textBlockRef} className="container">
-          {textArray.map((item, i) => {
-            return <TextItem key={i} text={item} />
-          })}
+      <div className="relative w-full h-full">
+        <div className="absolute inset-0 before:absolute before:bg-black/40 before:inset-0">
+          <VideoEmbed url="https://mytwynmediaservices-euno.akamaized.net/45f6339a-4429-44d6-a297-ef025d31558b/45f6339a-4429-44d6-a297-ef025d31.ism/manifest(format=m3u8-aapl).m3u8" />
         </div>
-      </motion.div>
-      <div className="flex items-center justify-center w-full h-full pt-20">
         <motion.div
-          className="w-64 md:w-[300px] relative z-10 will"
-          variants={{
-            hidden: {
-              opacity: 0,
-              scale: 0.5,
-            },
-            shown: {
-              opacity: 1,
-              scale: 1,
-            },
-          }}
-          initial="hidden"
+          ref={refTextHolder}
+          className="absolute bottom-0 z-10 w-full xl:px-20"
+          initial={{y: textBlockHeight}}
           transition={{
-            type: 'spring',
-            duration: 0.3,
+            duration: 0.2,
           }}
-          animate={controlsPhone}
+          style={{y: scrollText}}
         >
-          <Image
-            src="/images/iphone-frame-portrait.png"
-            width={580}
-            height={1171}
-            priority
-            alt="App"
-            className="absolute"
-          />
+          <div ref={textBlockRef} className="container">
+            {textArray.map((item, i) => {
+              return <TextItem key={i} text={item} />
+            })}
+          </div>
         </motion.div>
+        <div className="flex items-center justify-center w-full h-full pt-20">
+          <motion.div
+            className="w-64 md:w-[300px] relative z-10 will"
+            variants={{
+              hidden: {
+                opacity: 0,
+                scale: 0.5,
+              },
+              shown: {
+                opacity: 1,
+                scale: 1,
+              },
+            }}
+            initial="hidden"
+            transition={{
+              type: 'spring',
+              duration: 0.3,
+            }}
+            animate={controlsPhone}
+          >
+            <Image
+              src="/images/iphone-frame-portrait.png"
+              width={580}
+              height={1171}
+              priority
+              alt="App"
+              className="absolute"
+            />
+          </motion.div>
+        </div>
       </div>
     </section>
   )
