8452c0057b600c71b115414be2e1387661baa86b
DrShpongle <evgeniy.nagalskiy@gmail.com>
2022-02-02 11:55:16 +0200

    mobile version

--- src/components/sections/surf-app/visualise-and-do.tsx
+++ src/components/sections/surf-app/visualise-and-do.tsx
@@ -19,7 +19,7 @@ const VisualiseAndDo = () => {
   return (
     <section
       ref={refSection}
-      className="sticky top-0 h-screen py-12 overflow-hidden bg-white xl:py-16"
+      className="sticky top-0 h-screen pt-16 pb-5 overflow-hidden bg-white md:pb-8 2xl:pb-10 md:pt-20 lg:pt-20 xl:pt-24"
       style={{transform: 'translate3d(0,0,0)'}}
     >
       <div className="container flex flex-col h-full">
@@ -37,22 +37,37 @@ const VisualiseAndDo = () => {
         <div className="relative overflow-hidden grow">
           <div className="absolute inset-0">
             <div className="w-full h-full">
-              <div className="grid w-full h-full grid-cols-2">
-                <div className="bg-magenta-500">
+              <div className="grid w-full h-full md:grid-cols-2">
+                <div className="relative h-full bg-teal-500">
                   <VideoEmbed url="https://mytwynmediaservices-euno.akamaized.net/e703b314-1fbc-40c8-8055-e7bc86a78436/e703b314-1fbc-40c8-8055-e7bc86a7.ism/manifest(format=m3u8-aapl).m3u8" />
+                  <h3 className="absolute left-0 z-10 w-full text-5xl leading-none text-center text-white bottom-10 md:text-5xl lg:text-6xl xl:text-7xl">
+                    Bottom turn
+                  </h3>
                 </div>
-                <div className="bg-pink-500">
+                <div className="relative h-full bg-purple-500">
                   <VideoEmbed url="https://mytwynmediaservices-euno.akamaized.net/a095e6ac-0c83-4146-88d8-94305d057bd6/a095e6ac-0c83-4146-88d8-94305d05.ism/manifest(format=m3u8-aapl).m3u8" />
+                  <h3 className="absolute left-0 z-10 w-full text-5xl leading-none text-center text-white bottom-10 md:text-5xl lg:text-6xl xl:text-7xl">
+                    Top turn
+                  </h3>
                 </div>
               </div>
             </div>
-            <motion.div className="z-10 w-full h-full" style={{y: scrollText}}>
-              <div className="grid w-full h-full grid-cols-2">
-                <div className="bg-magenta-500">
+            <motion.div
+              className="relative z-10 w-full h-full"
+              style={{y: scrollText}}
+            >
+              <div className="grid w-full h-full md:grid-cols-2">
+                <div className="relative h-full bg-green-500">
                   <VideoEmbed url="https://mytwynmediaservices-euno.akamaized.net/a095e6ac-0c83-4146-88d8-94305d057bd6/a095e6ac-0c83-4146-88d8-94305d05.ism/manifest(format=m3u8-aapl).m3u8" />
+                  <h3 className="absolute left-0 z-10 w-full text-5xl leading-none text-center text-white bottom-10 md:text-5xl lg:text-6xl xl:text-7xl">
+                    Cut back
+                  </h3>
                 </div>
-                <div className="bg-pink-500">
+                <div className="relative h-full bg-orange-500">
                   <VideoEmbed url="https://mytwynmediaservices-euno.akamaized.net/e703b314-1fbc-40c8-8055-e7bc86a78436/e703b314-1fbc-40c8-8055-e7bc86a7.ism/manifest(format=m3u8-aapl).m3u8" />
+                  <h3 className="absolute left-0 z-10 w-full text-5xl leading-none text-center text-white bottom-10 md:text-5xl lg:text-6xl xl:text-7xl">
+                    Backside barrel
+                  </h3>
                 </div>
               </div>
             </motion.div>
