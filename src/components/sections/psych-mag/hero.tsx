4201853d5f8c42e24c31f5a845a03c898ebd92c3
DrShpongle <evgeniy.nagalskiy@gmail.com>
2022-02-10 11:01:05 +0200

    PsychMag hero section

--- src/components/sections/psych-mag/hero.tsx
+++ src/components/sections/psych-mag/hero.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import Image from 'next/image'
 import {Swiper, SwiperSlide} from 'swiper/react'
 
 import VideoPlayer from 'components/video-player'
@@ -7,18 +8,18 @@ import 'swiper/css'
 const Hero = () => {
   return (
     <section
-      className="bg-white pt-20 pb-12 md:pt-24 md:pb-20 lg:pt-28 xl:pt-36 xl:pb-24"
+      className="bg-white pt-20 pb-4 md:pt-24 md:pb-6 lg:pt-28 xl:pt-36 xl:pb-8"
       style={{transform: 'translate3d(0,0,0)'}}
     >
       <div className="container">
         <h2 className="text-center text-5xl leading-none md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-[111px]">
           Psych Mag
         </h2>
-        <p className="mx-auto mt-8 max-w-6xl text-center md:text-xl lg:mt-10 lg:text-2xl lg:leading-normal xl:mt-12 xl:text-3xl xl:leading-normal 2xl:text-[34px] 2xl:leading-normal">
+        <p className="mx-auto mt-8 max-w-6xl text-center md:text-xl lg:mt-10 lg:text-2xl lg:leading-normal xl:text-3xl xl:leading-normal 2xl:mt-12 2xl:text-[34px] 2xl:leading-normal">
           Expliqua sitibusa pe nullest, velitiust porerum vel escipsamusae nem
           nonsedit, utestiam, sus quia quis doluptio illatem et aut.
         </p>
-        <div className="relative mt-8 md:mt-10 lg:mt-16 xl:mt-24">
+        <div className="relative mt-8 aspect-video md:mt-10 lg:mt-12 xl:mt-20">
           <div className="absolute bottom-0 z-10 pl-3 pb-3 md:pl-6 md:pb-6 lg:pl-8 lg:pb-8 xl:pl-12 xl:pb-12">
             <h3 className="font-headings text-5xl leading-none text-white md:text-7xl lg:text-7xl xl:text-8xl 2xl:text-[111px] ">
               vlog.
@@ -31,26 +32,106 @@ const Hero = () => {
             controlsClasses="bottom-3 right-3 md:bottom-6 md:right-6 lg:bottom-8 lg:right-8 xl:bottom-12 xl:right-12"
           />
         </div>
+        <div className="mt-4 md:mt-6 2xl:mt-8">
+          <Swiper
+            spaceBetween={16}
+            slidesPerView={2}
+            onSlideChange={() => console.log('slide change')}
+            onSwiper={(swiper) => console.log(swiper)}
+            breakpoints={{
+              768: {
+                slidesPerView: 3,
+                spaceBetween: 20,
+              },
+              1024: {
+                slidesPerView: 3,
+                spaceBetween: 24,
+              },
+              1280: {
+                slidesPerView: 4,
+                spaceBetween: 32,
+              },
+            }}
+            loop
+          >
+            {fakeYoutubeVideosArr.map((item, i) => {
+              return (
+                <SwiperSlide>
+                  <div className="select-none">
+                    <a
+                      href={`https://www.youtube.com/watch?v=${item.id}`}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="relative block aspect-video"
+                    >
+                      <div className="absolute inset-0 overflow-hidden">
+                        <div className="relative h-full w-full duration-300 hover-hover:hover:scale-[1.15]">
+                          <Image
+                            src={`https://img.youtube.com/vi/${item.id}/maxresdefault.jpg`}
+                            alt={item.title}
+                            layout="fill"
+                            objectFit="cover"
+                          />
+                        </div>
+                      </div>
+                    </a>
+                    <div className="mt-3 flex w-full flex-col items-center space-y-1 md:mt-2 lg:mt-3 2xl:mt-4 2xl:space-y-2">
+                      <a
+                        href={`https://www.youtube.com/watch?v=${item.id}`}
+                        target="_blank"
+                        rel="noreferrer"
+                        className="relative block text-center font-headings text-xs text-pink line-clamp-1 lg:text-base xl:text-lg 2xl:text-xl"
+                      >
+                        {item.title}
+                      </a>
+                      <div className="flex flex-nowrap items-center text-xs lg:text-sm xl:text-base 2xl:text-lg">
+                        {item.date}
+                        <div className="mx-1 md:mx-2">|</div>
+                        {item.duration}
+                      </div>
+                    </div>
+                  </div>
+                </SwiperSlide>
+              )
+            })}
+          </Swiper>
+        </div>
       </div>
-      {/* <div>
-        <Swiper
-          spaceBetween={50}
-          slidesPerView={3}
-          onSlideChange={() => console.log('slide change')}
-          onSwiper={(swiper) => console.log(swiper)}
-          loop
-        >
-          <SwiperSlide>Slide 1</SwiperSlide>
-          <SwiperSlide>Slide 2</SwiperSlide>
-          <SwiperSlide>Slide 3</SwiperSlide>
-          <SwiperSlide>Slide 4</SwiperSlide>
-          <SwiperSlide>Slide 5</SwiperSlide>
-          <SwiperSlide>Slide 6</SwiperSlide>
-          <SwiperSlide>Slide 7</SwiperSlide>
-        </Swiper>
-      </div> */}
     </section>
   )
 }
 
 export default Hero
+
+const fakeYoutubeVideosArr = [
+  {
+    id: 'mRHG2lWvF4A',
+    title: 'THE MOST CROWDED PIPELINE EVER! (ITS GETTING WORSE)',
+    date: 'Jan 24, 2022',
+    duration: '16:51',
+  },
+  {
+    id: 'I6U6DmLJLB8',
+    title: 'MY WORST WIPE OUT AT PIPELINE!!! (LUCKY TO BE ALIVE)',
+    date: 'Jan 17, 2022',
+    duration: '16:43',
+  },
+  {
+    id: '23-5OsFdO9s',
+    title: 'GIRLS ATTEMPT DANGEROUS SLIP ‘N‘ SLIDE',
+    date: 'Dec 13, 2021',
+    duration: '12:57',
+  },
+  {
+    id: 'Uid6k5wPY68',
+    title: "JAMIE O'BRIEN PIPELINE HOUSE TOUR",
+    date: 'Apr 5, 2021',
+    duration: '24:18',
+  },
+  {
+    id: '7rXyTRsqSbA',
+    title: 'THIS IS PARADISE (HAWAII)',
+    date: 'May 25, 2020',
+    duration: '',
+  },
+]
