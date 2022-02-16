ca562a12c68832074c037e159f26ac401fc1556a
DrShpongle <evgeniy.nagalskiy@gmail.com>
2022-02-16 11:01:09 +0200

    adjust text color

--- src/components/sections/psych-mag/travel-guides.tsx
+++ src/components/sections/psych-mag/travel-guides.tsx
@@ -9,7 +9,7 @@ const TravelGuides = () => {
       id="travel-guides"
     >
       <div className="container">
-        <div className="flex w-full flex-col items-center space-y-5 text-center md:space-y-6 lg:space-y-7 xl:space-y-8">
+        <div className="flex w-full flex-col items-center space-y-5 text-center text-white md:space-y-6 lg:space-y-7 xl:space-y-8">
           <h2 className="text-5xl leading-none md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-[111px]">
             Travel Guides
           </h2>
