154914cbf1c588a0177498ccdbfc6bb50676e139
DrShpongle <evgeniy.nagalskiy@gmail.com>
2022-02-19 15:27:03 +0200

    article: wip

--- src/components/article.tsx
+++ src/components/article.tsx
@@ -7,7 +7,7 @@ const Article = ({blok}: any) => {
   console.log('blok:', blok)
   return (
     <section {...sbEditable(blok)} key={blok._uid}>
-      <div className="relative flex h-[60vh] flex-col justify-end px-44 py-28">
+      <div className="relative flex h-screen flex-col justify-end px-44 py-28">
         <div className="absolute inset-0">
           <Image
             layout="fill"
@@ -16,12 +16,12 @@ const Article = ({blok}: any) => {
             alt={blok.title}
           />
         </div>
-        <h2 className="relative whitespace-pre-wrap text-5xl leading-none text-white md:text-6xl lg:max-w-md lg:text-7xl xl:max-w-lg xl:text-8xl 2xl:max-w-2xl 2xl:text-[111px]">
+        <h2 className="relative text-5xl leading-none text-white md:text-6xl lg:max-w-md lg:text-7xl xl:max-w-lg xl:text-8xl 2xl:max-w-4xl 2xl:text-[111px]">
           {blok.title}
         </h2>
       </div>
       <div className="bg-blue px-44 py-28 text-4xl text-white">
-        {blok.description}
+        <div className="max-w-6xl">{blok.description}</div>
       </div>
       <div className="columns-2 gap-32 px-44 py-28">{render(blok.content)}</div>
     </section>
