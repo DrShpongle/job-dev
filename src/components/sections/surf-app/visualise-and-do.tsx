e49d4574d631ce8a57a3de6527da675eeb97f8de
DrShpongle <evgeniy.nagalskiy@gmail.com>
2022-02-23 13:51:42 +0200

    Stroyblok: VisualiseAndDo

--- src/components/sections/surf-app/visualise-and-do.tsx
+++ src/components/sections/surf-app/visualise-and-do.tsx
@@ -4,7 +4,7 @@ import {motion, useViewportScroll, useTransform} from 'framer-motion'
 import {useRefScrollProgress} from 'hooks/useRefScrollProgress'
 import VideoEmbed from 'components/video-embed'
 
-const VisualiseAndDo = () => {
+const VisualiseAndDo = ({blok}: any) => {
   const refSection = React.useRef<HTMLDivElement>(null)
 
   const {start, end} = useRefScrollProgress(refSection)
@@ -25,13 +25,10 @@ const VisualiseAndDo = () => {
         <div className="container flex h-full flex-col">
           <div className="mb-6 shrink-0 md:mb-12">
             <h2 className="text-5xl leading-none md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-[111px]">
-              Visualise and do
+              {blok.title}
             </h2>
             <p className="mt-4 md:mt-8 md:text-xl lg:text-2xl lg:leading-normal xl:max-w-4xl xl:text-3xl xl:leading-normal 2xl:max-w-5xl">
-              Jamie improved by watching clips of his favourite surfers over and
-              over and over again. The JOB Surf app has technique specific clip
-              rolls. You want to watch Jamie land his signature backside barrels
-              on repeat so YOU can replicate? We have got you covered!
+              {blok.description}
             </p>
           </div>
           <div className="relative grow overflow-hidden">
@@ -39,15 +36,15 @@ const VisualiseAndDo = () => {
               <div className="h-full w-full">
                 <div className="grid h-full w-full md:grid-cols-2">
                   <div className="relative h-full">
-                    <VideoEmbed url="https://mytwynmediaservices-euno.akamaized.net/e703b314-1fbc-40c8-8055-e7bc86a78436/e703b314-1fbc-40c8-8055-e7bc86a7.ism/manifest(format=m3u8-aapl).m3u8" />
+                    <VideoEmbed url={blok.video_top_left_url.url} />
                     <h3 className="absolute left-0 bottom-6 z-10 w-full text-center text-4xl leading-none text-white md:bottom-10 md:text-5xl lg:text-6xl xl:text-7xl">
-                      Bottom turn
+                      {blok.video_top_left_title}
                     </h3>
                   </div>
                   <div className="relative h-full">
-                    <VideoEmbed url="https://mytwynmediaservices-euno.akamaized.net/a095e6ac-0c83-4146-88d8-94305d057bd6/a095e6ac-0c83-4146-88d8-94305d05.ism/manifest(format=m3u8-aapl).m3u8" />
+                    <VideoEmbed url={blok.video_top_right_url.url} />
                     <h3 className="absolute left-0 bottom-6 z-10 w-full text-center text-4xl leading-none text-white md:bottom-10 md:text-5xl lg:text-6xl xl:text-7xl">
-                      Top turn
+                      {blok.video_top_right_title}
                     </h3>
                   </div>
                 </div>
@@ -58,15 +55,15 @@ const VisualiseAndDo = () => {
               >
                 <div className="grid h-full w-full md:grid-cols-2">
                   <div className="relative h-full">
-                    <VideoEmbed url="https://mytwynmediaservices-euno.akamaized.net/a095e6ac-0c83-4146-88d8-94305d057bd6/a095e6ac-0c83-4146-88d8-94305d05.ism/manifest(format=m3u8-aapl).m3u8" />
+                    <VideoEmbed url={blok.video_bottom_left_url.url} />
                     <h3 className="absolute left-0 bottom-6 z-10 w-full text-center text-4xl leading-none text-white md:bottom-10 md:text-5xl lg:text-6xl xl:text-7xl">
-                      Cut back
+                      {blok.video_bottom_left_title}
                     </h3>
                   </div>
                   <div className="relative h-full">
-                    <VideoEmbed url="https://mytwynmediaservices-euno.akamaized.net/e703b314-1fbc-40c8-8055-e7bc86a78436/e703b314-1fbc-40c8-8055-e7bc86a7.ism/manifest(format=m3u8-aapl).m3u8" />
+                    <VideoEmbed url={blok.video_bottom_right_url.url} />
                     <h3 className="absolute left-0 bottom-6 z-10 w-full text-center text-4xl leading-none text-white md:bottom-10 md:text-5xl lg:text-6xl xl:text-7xl">
-                      Backside barrel
+                      {blok.video_bottom_right_title}
                     </h3>
                   </div>
                 </div>
