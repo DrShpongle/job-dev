09c3cbd2f083b3a4178aeae34d068a59b1390846
DrShpongle <evgeniy.nagalskiy@gmail.com>
2022-02-23 12:45:33 +0200

    Storyblok: AskJamie

--- src/components/sections/surf-app/ask-jamie.tsx
+++ src/components/sections/surf-app/ask-jamie.tsx
@@ -4,7 +4,8 @@ import Image from 'next/image'
 import VideoPlayer from 'components/video-player'
 import {IconPlay, IconPause, IconVolumeOn, IconVolumeOff} from 'lib/icons'
 
-const AskJamie = () => {
+const AskJamie = ({blok}: any) => {
+  console.log('blok:', blok)
   const [playing, setPlaying] = React.useState(true)
   const [muted, setMuted] = React.useState(true)
   return (
@@ -14,18 +15,15 @@ const AskJamie = () => {
     >
       <div className="container">
         <h2 className="text-5xl leading-none md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-[111px]">
-          Ask Jamie
+          {blok.title}
         </h2>
         <p className="mt-4 md:mt-8 md:text-xl lg:text-2xl lg:leading-normal xl:max-w-4xl xl:text-3xl xl:leading-normal 2xl:max-w-5xl 2xl:text-[34px] 2xl:leading-normal">
-          If you’ve always wanted to ask JOB a question, this is for you.
-          Whether it’s how to stay so focused, overcome bad habits or what do I
-          need to do to create loads of spray with my top turn? ASK AWAY. You
-          literally have Jamie in your pocket!
+          {blok.description}
         </p>
         <div className="relative mt-8 flex flex-col items-center md:mt-10 md:block md:pb-16 lg:mt-12 lg:pb-14 xl:mt-16 xl:pl-20 xl:pb-16 2xl:pb-20">
           <div className="mt-6 hidden w-[80%] md:block xl:mt-0 xl:w-[996px] 2xl:w-[1220px]">
             <Image
-              src="/images/ask-jamie.jpg"
+              src={blok.image.filename}
               width={1220}
               height={674}
               alt="Ask Jamie"
@@ -38,7 +36,7 @@ const AskJamie = () => {
               style={{transform: 'translateZ(0)'}}
             >
               <VideoPlayer
-                url="https://mytwynmediaservices-euno.akamaized.net/e6a22efa-526b-468f-a6e8-172f3901c6cf/e6a22efa-526b-468f-a6e8-172f3901.ism/manifest(format=m3u8-aapl).m3u8"
+                url={blok.video.url}
                 controlsClasses="bottom-4 right-12 md:bottom-8 md:right-24 lg:bottom-14 lg:right-36 xl:bottom-16 xl:right-44"
                 externalControls={true}
                 playing={playing}
