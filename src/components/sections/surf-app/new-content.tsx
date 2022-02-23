c7dbdfa8397e869760d1936e19bb1a583fa9f116
DrShpongle <evgeniy.nagalskiy@gmail.com>
2022-02-23 14:46:55 +0200

    Storyblok: NewContent

--- src/components/sections/surf-app/new-content.tsx
+++ src/components/sections/surf-app/new-content.tsx
@@ -6,10 +6,11 @@ import {
   useTransform,
   useAnimation,
 } from 'framer-motion'
+import {render} from 'storyblok-rich-text-react-renderer'
 
 import {useRefScrollProgress} from 'hooks/useRefScrollProgress'
 
-const NewContent = () => {
+const NewContent = ({blok}: any) => {
   const refSection = React.useRef<HTMLDivElement>(null)
 
   const {start, end} = useRefScrollProgress(refSection)
@@ -92,17 +93,11 @@ const NewContent = () => {
             <div className="w-full overflow-hidden text-white">
               <div className="container flex flex-col items-center">
                 <h2 className="text-5xl leading-none md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-[111px]">
-                  New Content
+                  {blok.title}
                 </h2>
-                <p className="mt-4 md:mt-8 md:text-xl lg:text-2xl lg:leading-normal xl:max-w-4xl xl:text-3xl xl:leading-normal 2xl:max-w-5xl 2xl:text-[34px] 2xl:leading-normal">
-                  Jamie and friends will be dropping throughout the year,
-                  everything from specific lessons right through to new answers
-                  to the most asked questions. We are constantly working to help
-                  you improve, stay psyched and get more out of your surfing.
-                  <br />
-                  <br />
-                  Beautifully shot in ultra high def.
-                </p>
+                <div className="mt-4 md:mt-8 md:text-xl lg:text-2xl lg:leading-normal xl:max-w-4xl xl:text-3xl xl:leading-normal 2xl:max-w-5xl 2xl:text-[34px] 2xl:leading-normal">
+                  {render(blok.description)}
+                </div>
               </div>
             </div>
           </div>
