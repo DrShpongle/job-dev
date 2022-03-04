9ca169134b6d5cc483b839a0b7d5e2b68cccbf15
DrShpongle <evgeniy.nagalskiy@gmail.com>
2022-03-04 12:47:02 +0200

    formatting

--- src/components/sections/teaser/teaser-hero.tsx
+++ src/components/sections/teaser/teaser-hero.tsx
@@ -1,16 +1,13 @@
 import * as React from 'react'
 import Image from 'next/image'
-import {
-  motion,
-  useAnimation,
-} from 'framer-motion'
+import {motion, useAnimation} from 'framer-motion'
 
 import VideoPlayer from 'components/video-player'
 
 const TeaserHero: React.FC<any> = ({blok}) => {
   const refSection = React.useRef<HTMLDivElement>(null)
   const controlsPhone = useAnimation()
-  
+
   return (
     <section className="pt-36" style={{transform: 'translateZ(0)'}}>
       <div className="absolute inset-0 z-[-1] h-full">
@@ -25,19 +22,25 @@ const TeaserHero: React.FC<any> = ({blok}) => {
         </div>
       </div>
       <div className="text-center">
-        <h2 className="px-4 text-white text-7xl">Psyched for the Release?</h2>
-        <p className="px-4 text-2xl text-white pt-4">We&apos;re loading up the app with Jamie&apos;s radest videos and tutorials.</p>
-        <p className="px-4 text-2xl text-white">Sign up below and we&apos;ll let you know the second it hits the app store.</p>
+        <h2 className="px-4 text-7xl text-white">Psyched for the Release?</h2>
+        <p className="px-4 pt-4 text-2xl text-white">
+          We&apos;re loading up the app with Jamie&apos;s radest videos and
+          tutorials.
+        </p>
+        <p className="px-4 text-2xl text-white">
+          Sign up below and we&apos;ll let you know the second it hits the app
+          store.
+        </p>
       </div>
       <div className="flex h-full w-full items-center justify-center pt-8">
         <div
           className="md:w-[36rem] lg:w-[38rem] xl:w-[44rem] 2xl:w-[54rem] landscape:w-[24rem] md:landscape:w-[36rem] lg:landscape:w-[38rem] xl:landscape:w-[44rem] 2xl:landscape:w-[54rem]"
           style={{transform: 'translateZ(0)'}}
         >
-          <div className="absolute w-full h-full pt-2 pr-4 pb-4">
-            <div className="w-full h-full overflow-hidden rounded-[75px]">
+          <div className="absolute h-full w-full pt-2 pr-4 pb-4">
+            <div className="h-full w-full overflow-hidden rounded-[75px]">
               <VideoPlayer
-                url="https://mytwyn-global.akamaized.net/877053fb-0352-40e7-a327-bb6563bcde36/877053fb-0352-40e7-a327-bb6563bc.ism/manifest(format=m3u8-aapl).m3u8" 
+                url="https://mytwyn-global.akamaized.net/877053fb-0352-40e7-a327-bb6563bcde36/877053fb-0352-40e7-a327-bb6563bc.ism/manifest(format=m3u8-aapl).m3u8"
                 controls={true}
                 muted={false}
                 playing={true}
@@ -51,11 +54,13 @@ const TeaserHero: React.FC<any> = ({blok}) => {
             height={580}
             alt="Ask Jamie"
             priority
-           />
+          />
         </div>
       </div>
       <div className="text-center">
-        <h4 className="px-4 text-2xl text-white pt-12 pb-8">Scroll down to register for the sickest surf app</h4>
+        <h4 className="px-4 pt-12 pb-8 text-2xl text-white">
+          Scroll down to register for the sickest surf app
+        </h4>
       </div>
     </section>
   )
--- src/components/sections/teaser/teaser-sign-up.tsx
+++ src/components/sections/teaser/teaser-sign-up.tsx
@@ -9,43 +9,56 @@ const TeaserSignUp: React.FC<any> = ({blok}) => {
     >
       <div className="container flex flex-col justify-center">
         <div>
-          <h3 className="font-accented text-5xl text-white">
-            Need The App?
-          </h3>
-          <h2 className="mt-1 text-7xl leading-none text-white max-w-4xl">
+          <h3 className="font-accented text-5xl text-white">Need The App?</h3>
+          <h2 className="mt-1 max-w-4xl text-7xl leading-none text-white">
             drop us your deets
           </h2>
-          <p className="text-2xl text-white pt-4">Pre-register and get in early on Jamie&apos;s epic Surf App.</p>
+          <p className="pt-4 text-2xl text-white">
+            Pre-register and get in early on Jamie&apos;s epic Surf App.
+          </p>
         </div>
         <form>
-          <div className="grid grid-cols-1 md:grid-cols-3 pt-4">
-            <div><input
-              type="text"
-              className="h-[48px] rounded-none w-full md:px-5 md:text-xl 2xl:text-2xl"
-              placeholder="Jamie"
-            /></div>
-            <div className="pt-4 md:pt-0 md:pl-4"><input
-              type="text"
-              className="h-[48px] rounded-none w-full md:px-5 md:text-xl 2xl:text-2xl"
-              placeholder="O'Brien"
-            /></div>
-            <div className="pt-4 md:pt-0 md:pl-4"><input
-              type="text"
-              className="h-[48px] rounded-none w-full md:px-5 md:text-xl 2xl:text-2xl"
-              placeholder="legend@job.com"
-            /></div>
-            <div className="md:col-span-2 pt-4 flex">
+          <div className="grid grid-cols-1 pt-4 md:grid-cols-3">
+            <div>
+              <input
+                type="text"
+                className="h-[48px] w-full rounded-none md:px-5 md:text-xl 2xl:text-2xl"
+                placeholder="Jamie"
+              />
+            </div>
+            <div className="pt-4 md:pt-0 md:pl-4">
+              <input
+                type="text"
+                className="h-[48px] w-full rounded-none md:px-5 md:text-xl 2xl:text-2xl"
+                placeholder="O'Brien"
+              />
+            </div>
+            <div className="pt-4 md:pt-0 md:pl-4">
+              <input
+                type="text"
+                className="h-[48px] w-full rounded-none md:px-5 md:text-xl 2xl:text-2xl"
+                placeholder="legend@job.com"
+              />
+            </div>
+            <div className="flex pt-4 md:col-span-2">
               <div>
-                <label className="text-white text-lg">
-                  Add a check to agree to us storing your details. We need this to be able to tell you when the app is ready and we&apos;ll occasionally hit your inbox with the latest on JOB.
+                <label className="text-lg text-white">
+                  Add a check to agree to us storing your details. We need this
+                  to be able to tell you when the app is ready and we&apos;ll
+                  occasionally hit your inbox with the latest on JOB.
                 </label>
               </div>
-              <div className="w-[48px] h-[48px] shrink-0">
-                <input className="w-[48px] h-[48px] opacity-0 absolute" type="checkbox" />
-                <div className="w-full h-full bg-white text-center"><p className="text-blue text-5xl">✓</p></div>
+              <div className="h-[48px] w-[48px] shrink-0">
+                <input
+                  className="absolute h-[48px] w-[48px] opacity-0"
+                  type="checkbox"
+                />
+                <div className="h-full w-full bg-white text-center">
+                  <p className="text-5xl text-blue">✓</p>
+                </div>
               </div>
             </div>
-            <div className="pt-4 md:pl-4" >
+            <div className="pt-4 md:pl-4">
               <button
                 type="button"
                 className="h-[48px] w-full bg-blue text-center font-headings text-lg uppercase text-white md:text-xl xl:text-[1.625rem]"
@@ -57,7 +70,7 @@ const TeaserSignUp: React.FC<any> = ({blok}) => {
         </form>
         <div className="pt-12">
           <Image
-	    width="2500"
+            width="2500"
             height="1662"
             layout="responsive"
             src="/images/teaser-sign-up.jpg"
