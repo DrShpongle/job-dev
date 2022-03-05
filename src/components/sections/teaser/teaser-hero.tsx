f8fd37f00451f7452ae9bbe35b070d2158f12fa8
DrShpongle <evgeniy.nagalskiy@gmail.com>
2022-03-05 10:33:53 +0200

    tweaks

--- src/components/sections/teaser/teaser-hero.tsx
+++ src/components/sections/teaser/teaser-hero.tsx
@@ -34,11 +34,11 @@ const TeaserHero: React.FC<any> = ({blok}) => {
       </div>
       <div className="flex h-full w-full items-center justify-center pt-8">
         <div
-          className="md:w-[36rem] lg:w-[38rem] xl:w-[44rem] 2xl:w-[54rem] landscape:w-[24rem] md:landscape:w-[36rem] lg:landscape:w-[38rem] xl:landscape:w-[44rem] 2xl:landscape:w-[54rem]"
+          className="w-80 md:w-[36rem] lg:w-[38rem] xl:w-[44rem] 2xl:w-[54rem] landscape:w-[24rem] md:landscape:w-[36rem] lg:landscape:w-[38rem] xl:landscape:w-[44rem] 2xl:landscape:w-[54rem]"
           style={{transform: 'translateZ(0)'}}
         >
-          <div className="absolute h-full w-full pt-2 pr-4 pb-4">
-            <div className="h-full w-full overflow-hidden rounded-[75px]">
+          <div className="absolute h-full w-full pt-2 pb-4 md:pr-4">
+            <div className="h-full w-full overflow-hidden rounded-[30px] md:rounded-[40px] xl:rounded-[60px] 2xl:rounded-[75px]">
               <VideoPlayer
                 playing={true}
                 url="https://mytwyn-global.akamaized.net/877053fb-0352-40e7-a327-bb6563bcde36/877053fb-0352-40e7-a327-bb6563bc.ism/manifest(format=m3u8-aapl).m3u8"
