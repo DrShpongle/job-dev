33fe21c5a20bbd8e161c0060f6025fb5a3230b39
DrShpongle <evgeniy.nagalskiy@gmail.com>
2022-04-06 20:25:29 +0300

    add FB script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -37,6 +37,29 @@ class MyDocument extends Document {
           <script
             dangerouslySetInnerHTML={{
               __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window,document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '671463990803677'); 
+              fbq('track', 'PageView');
+          `,
+            }}
+          />
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `
+              <img height="1" width="1" src="https://www.facebook.com/tr?id=671463990803677&ev=PageView&noscript=1"/>
+          `,
+            }}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
               var _urconfig = { sid: "7db37aa7-69a0-46db-91c5-4c651977a521", aip: 0, usePageProtocol: false };
               (function (d, s) {
                   var js = d.createElement(s),
