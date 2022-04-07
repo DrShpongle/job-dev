3dd177c74e3d7aadfcde6c8dc5ddddb6be915428
DrShpongle <evgeniy.nagalskiy@gmail.com>
2022-04-07 17:19:45 +0300

    revert the changes

--- src/components/sections/surf-app/hero-with-scrolling-text.tsx
+++ src/components/sections/surf-app/hero-with-scrolling-text.tsx
@@ -1,7 +1,12 @@
 import * as React from 'react'
 import Image from 'next/image'
 import {sbEditable} from '@storyblok/storyblok-editable'
-import {motion, useViewportScroll, useTransform} from 'framer-motion'
+import {
+  motion,
+  useViewportScroll,
+  useTransform,
+  useAnimation,
+} from 'framer-motion'
 import {useMeasure} from 'react-use'
 import classNames from 'classnames'
 
@@ -54,11 +59,11 @@ const TextItem: React.FC<{text: string; firstItem: boolean}> = ({
 
 const HeroWithScrollableText = ({blok}: any) => {
   const [isMounted, setIsMounted] = React.useState<boolean>(false)
-  const [triggerPhoneAnimation, setTriggerPhoneAnimation] =
-    React.useState(false)
   const refSection = React.useRef<HTMLDivElement>(null)
   const refTextHolder = React.useRef<HTMLDivElement>(null)
   const [textBlockRef, {height: textBlockHeight}] = useMeasure<any>()
+
+  const controlsPhone = useAnimation()
 
   const {start, end} = useRefScrollProgress(refSection)
   const {scrollYProgress} = useViewportScroll()
@@ -80,18 +85,18 @@ const HeroWithScrollableText = ({blok}: any) => {
   }, [])
 
   useIsomorphicLayoutEffect(() => {
-    const phoneAnimation = () => {
+    const triggerPhoneAnimation = () => {
       if (scrollText.get() < -windowHeight * 0.5) {
-        setTriggerPhoneAnimation(true)
+        controlsPhone.start('shown')
       } else {
-        setTriggerPhoneAnimation(false)
+        controlsPhone.start('hidden')
       }
     }
-    const unsubscribeY = scrollText.onChange(phoneAnimation)
+    const unsubscribeY = scrollText.onChange(triggerPhoneAnimation)
     return () => {
       unsubscribeY()
     }
-  }, [triggerPhoneAnimation])
+  }, [])
 
   return (
     <section ref={refSection} {...sbEditable(blok)} key={blok._uid}>
@@ -140,7 +145,7 @@ const HeroWithScrollableText = ({blok}: any) => {
               type: 'spring',
               duration: 0.3,
             }}
-            animate={triggerPhoneAnimation ? 'shown' : 'hidden'}
+            animate={controlsPhone}
           >
             <div className="relative md:w-[36rem] lg:w-[38rem] xl:w-[44rem] 2xl:w-[54rem] portrait:w-64 md:portrait:w-[36rem] lg:portrait:w-[38rem] xl:portrait:w-[44rem] 2xl:portrait:w-[54rem] landscape:w-[24rem] md:landscape:w-[36rem] lg:landscape:w-[38rem] xl:landscape:w-[44rem] 2xl:landscape:w-[54rem]">
               <div className="border-radius-fix absolute inset-2 overflow-hidden rounded-[30px] md:inset-3 xl:rounded-[40px] 2xl:rounded-[50px]">
