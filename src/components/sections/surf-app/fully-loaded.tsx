b8790477d768559ada8887583aa32ca4d3139d13
DrShpongle <evgeniy.nagalskiy@gmail.com>
2022-04-08 16:22:11 +0300

    tweaks

--- src/components/sections/surf-app/fully-loaded.tsx
+++ src/components/sections/surf-app/fully-loaded.tsx
@@ -28,7 +28,7 @@ const MotionItem: React.FC<{url: string}> = ({url}) => {
     >
       <div className="relative md:w-[36rem] lg:w-[38rem] xl:w-[44rem] 2xl:w-[54rem] portrait:w-64 md:portrait:w-[36rem] lg:portrait:w-[38rem] xl:portrait:w-[44rem] 2xl:portrait:w-[54rem] landscape:w-[24rem] md:landscape:w-[36rem] lg:landscape:w-[38rem] xl:landscape:w-[44rem] 2xl:landscape:w-[54rem]">
         <div className="border-radius-fix absolute inset-2 overflow-hidden rounded-[30px] md:inset-3 xl:rounded-[40px] 2xl:rounded-[50px]">
-          <VideoPlayer playing={true} url={url} controls={true} muted={true} />
+          <VideoPlayer playing={false} url={url} controls={true} muted={true} />
         </div>
         <div className="pointer-events-none portrait:hidden md:portrait:block">
           <Image
