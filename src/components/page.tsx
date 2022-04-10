08f0695c78b2ff6468fb0758f6ea794c82468b67
DrShpongle <evgeniy.nagalskiy@gmail.com>
2022-04-10 19:44:06 +0300

    js --> ts

--- src/components/page.tsx
+++ src/components/page.tsx
@@ -1,10 +1,10 @@
 import PageLayout from 'components/layouts/page-layout'
 import DynamicComponent from './dynamic-component'
 
-const Page = ({blok}) => (
+const Page = ({blok}: any) => (
   <PageLayout>
     {blok.body
-      ? blok.body.map((blok) => (
+      ? blok.body.map((blok: any) => (
           <DynamicComponent blok={blok} key={blok._uid} />
         ))
       : null}
