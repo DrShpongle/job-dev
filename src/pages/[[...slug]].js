e9b1e004877bb6c7443cef9586aa1e9eac15ef47
DrShpongle <evgeniy.nagalskiy@gmail.com>
2022-04-10 16:03:49 +0300

    seo for the articles

--- src/pages/[[...slug]].js
+++ src/pages/[[...slug]].js
@@ -7,18 +7,31 @@ export default function Page(props) {
   const {story, preview} = props
   const enableBridge = true
   const actualStory = useStoryblok(story, enableBridge)
-  const seoObj = actualStory.content?.seo?.[0]
-  const seo = isEmpty(seoObj)
-    ? {}
-    : Object.keys(seoObj)
-        .filter(
-          (key) => key !== '_editable' && key !== '_uid' && key !== 'component',
-        )
-        .reduce((obj, key) => {
-          return Object.assign(obj, {
-            [key]: seoObj[key],
-          })
-        }, {})
+  const seoPage =
+    actualStory.content.component === 'page'
+      ? actualStory.content?.seo?.[0]
+      : {}
+  const seoArticle =
+    actualStory.content.component === 'article'
+      ? {
+          title: actualStory.content.title,
+          description: actualStory.content.short_description,
+          image: actualStory.content.hero_image,
+        }
+      : {}
+
+  let seo = {}
+
+  switch (actualStory.content.component) {
+    case 'page':
+      seo = seoPage
+      break
+    case 'article':
+      seo = seoArticle
+      break
+    default:
+      seo = {}
+  }
 
   return (
     <>
@@ -28,12 +41,12 @@ export default function Page(props) {
         openGraph={{
           title: seo.title,
           description: seo.description,
-          url: `${process.env.NEXT_PUBLIC_DEPLOYMENT_URL}/${actualStory.slug}`,
+          url: `${process.env.NEXT_PUBLIC_DEPLOYMENT_URL}${
+            actualStory.content.component === 'article' ? '/articles/' : '/'
+          }${actualStory.slug}`,
           images: [
             {
-              // TODO
-              // url: `${process.env.NEXT_PUBLIC_DEPLOYMENT_URL}/images/og-image.png`,
-              url: 'https://job-dev.vercel.app/images/og-image.png',
+              url: seo.image.filename,
             },
           ],
         }}
