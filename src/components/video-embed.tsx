4281e32bac825856e1fa8c6db9a5034d207f5529
DrShpongle <evgeniy.nagalskiy@gmail.com>
2022-04-12 14:34:53 +0300

    play/pause for the embed videos

--- src/components/video-embed.tsx
+++ src/components/video-embed.tsx
@@ -1,28 +1,40 @@
 import * as React from 'react'
 import ReactPlayer from 'react-player'
+import Observer from '@researchgate/react-intersection-observer'
 
-const VideoEmbed: React.FC<{url: string; className?: string}> = ({url}) => (
-  <>
-    <div className="embed-video-wrapper relative h-full w-full">
-      <ReactPlayer
-        playsinline
-        playing
-        muted
-        loop
-        url={url}
-        width="100%"
-        height="100%"
-        config={{
-          file: {
-            hlsOptions: {
-              abrBandWidthUpFactor: 0.5,
-              startLevel: 3,
+const VideoEmbed: React.FC<{
+  url: string
+}> = ({url}) => {
+  const [playing, setPlaying] = React.useState(false)
+  const handleIntersection = (event: any) => {
+    setPlaying(event.isIntersecting)
+  }
+  const options = {
+    onChange: handleIntersection,
+  }
+  return (
+    <Observer {...options}>
+      <div className="embed-video-wrapper relative h-full w-full">
+        <ReactPlayer
+          playsinline
+          playing={playing}
+          muted
+          loop
+          url={url}
+          width="100%"
+          height="100%"
+          config={{
+            file: {
+              hlsOptions: {
+                abrBandWidthUpFactor: 0.5,
+                startLevel: 3,
+              },
             },
-          },
-        }}
-      />
-    </div>
-  </>
-)
+          }}
+        />
+      </div>
+    </Observer>
+  )
+}
 
 export default VideoEmbed
