bac3985e35af722fec0a83921a00719ae48b2072
DrShpongle <evgeniy.nagalskiy@gmail.com>
2022-04-24 15:45:59 +0300

    spped test

--- src/components/sections/home/vlog.tsx
+++ src/components/sections/home/vlog.tsx
@@ -28,7 +28,7 @@ const Vlog: React.FC<any> = ({blok}) => {
               width={1283}
               height={1118}
               alt="tv frame"
-              priority
+              // priority
             />
           </div>
         </div>
