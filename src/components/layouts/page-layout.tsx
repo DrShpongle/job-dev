bad65a51eb7d7185cf16f7089df66cc3f948f2dd
DrShpongle <evgeniy.nagalskiy@gmail.com>
2022-05-03 21:54:17 +0300

    seatrees section is not shown on SeaTrees page

--- src/components/layouts/page-layout.tsx
+++ src/components/layouts/page-layout.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import {useRouter} from 'next/router'
 
 import Header from 'components/header'
 import Subheader from 'components/subheader'
@@ -7,13 +8,15 @@ import Partnered from 'components/sections/partnered'
 import Footer from 'components/footer'
 
 const PageLayout: React.FC<{children: React.ReactNode}> = ({children}) => {
+  const router = useRouter()
+  const matchString = /^\/sea-trees/
   return (
     <div className="">
       <Header />
       <Subheader />
       {children}
       <div style={{transform: 'translate3d(0,0,0)'}}>
-        <SeaTrees />
+        {!matchString.test(router.asPath) && <SeaTrees />}
         <Partnered />
         <Footer />
       </div>
