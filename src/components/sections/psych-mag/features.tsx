1ad39b289374898cac40ca80f1b792140694925a
DrShpongle <evgeniy.nagalskiy@gmail.com>
2022-05-03 21:36:47 +0300

    refactor utility

--- src/components/sections/psych-mag/features.tsx
+++ src/components/sections/psych-mag/features.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import {isEmpty} from 'lodash'
 
-import {getScrolledToAnchor} from 'utils/get-scrolled-to-anchor'
+import {scrollToAnchor} from 'utils/scroll-to-anchor'
 import {getArticlesByCategory} from 'utils/get-articles'
 import Card from 'components/card'
 
@@ -14,7 +14,7 @@ const Features = ({blok}: any) => {
     )
   }, [])
   React.useEffect(() => {
-    getScrolledToAnchor('features')
+    scrollToAnchor('features')
   }, [])
   return (
     <section
