7ea75b851f9c3c5e3c3d64600f6aa082282b7474
DrShpongle <evgeniy.nagalskiy@gmail.com>
2022-05-10 16:16:15 +0300

    remove SeaTrees

--- src/components/layouts/page-layout.tsx
+++ src/components/layouts/page-layout.tsx
@@ -1,22 +1,22 @@
 import * as React from 'react'
-import {useRouter} from 'next/router'
+// import {useRouter} from 'next/router'
 
 import Header from 'components/header'
 import Subheader from 'components/subheader'
-import SeaTrees from 'components/sections/sea-trees'
+// import SeaTrees from 'components/sections/sea-trees'
 import Partnered from 'components/sections/partnered'
 import Footer from 'components/footer'
 
 const PageLayout: React.FC<{children: React.ReactNode}> = ({children}) => {
-  const router = useRouter()
-  const matchString = /^\/sea-trees/
+  // const router = useRouter()
+  // const matchString = /^\/sea-trees/
   return (
     <div>
       <Header />
       <Subheader />
       {children}
       <div style={{transform: 'translate3d(0,0,0)'}}>
-        {!matchString.test(router.asPath) && <SeaTrees />}
+        {/* {!matchString.test(router.asPath) && <SeaTrees />} */}
         <Partnered />
         <Footer />
       </div>
